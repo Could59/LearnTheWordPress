a53d0d1cb07acf59ed88a71f080308040098048e
Could59 <32735002+Could59@users.noreply.github.com>
2017-11-28 10:28:10 +0100

    manges mes pieds

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Ici, je suis sur le fichier par défaut pour tout théme Wp.
+Il est l'un des 2 obligatoire (l'autre étant style.css)
+Il est utilisé pour afficher une page si aucun autre fichier n'est disponible
+https://developper.wordpress.org/themes/basics/template-hierarchy/
+*/
+get_header();
+get_footer();
+?>
+
+
+
